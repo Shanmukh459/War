8d01b895f0d3ac1dd1af11a1af8750299e83210b
Shanmukh459 <saikrrish459@gmail.com>
2024-01-25 12:13:51 -0800

    added a new button to draw cards and validate the deckID while drawing

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-/**
- Challenge: Add a button that, when clicked, gets a new deck of cards from the deckofcards API
- 
- URL: https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/
- 
- Log the whole response to the console
- */
+let deckId = ''
+document.getElementById("new-deck").addEventListener('click', function() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            deckId = data.deck_id
+        })
+})
 
+document.getElementById("draw-cards").addEventListener('click', function() {
+    if(deckId) {
+        fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
+    else {
+        console.log("Please draw a deck before drawing cards!")
+    }
+})
