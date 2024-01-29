6012c712df2423ef32dc21830123657e931fc75a
Shanmukh459 <saikrrish459@gmail.com>
2024-01-29 13:04:01 -0800

    added basic css

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ document.getElementById("draw-cards").addEventListener('click', function() {
     if(deckId) {
         fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => {
+                document.getElementById("card1").src = data.cards[0].image
+                document.getElementById("card2").src = data.cards[1].image
+
+            })
     }
     else {
         console.log("Please draw a deck before drawing cards!")
