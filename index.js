77debdc009a6454453d626dcf8e6d4db61c518db
Shanmukh459 <saikrrish459@gmail.com>
2024-01-30 18:56:44 -0800

    display cards on screen and which card is highest

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const cards = document.getElementById("cards") 
+const newDeck = document.getElementById("new-deck")
+const drawCardsBtn = document.getElementById("draw-cards")
+const result = document.getElementById("result")
+
 let deckId = ''
-document.getElementById("new-deck").addEventListener('click', function() {
+
+newDeck.addEventListener('click', function() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
@@ -8,17 +14,35 @@ document.getElementById("new-deck").addEventListener('click', function() {
         })
 })
 
-document.getElementById("draw-cards").addEventListener('click', function() {
+drawCardsBtn.addEventListener('click', function() {
     if(deckId) {
         fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
             .then(res => res.json())
             .then(data => {
-                document.getElementById("card1").src = data.cards[0].image
-                document.getElementById("card2").src = data.cards[1].image
-
+                const card1 = data.cards[0]
+                const card2 = data.cards[1]
+                cards.children[0].src = card1.image
+                cards.children[1].src = card2.image
+                const resultText = compareCards(card1, card2)
+                result.textContent = resultText
             })
     }
     else {
         console.log("Please draw a deck before drawing cards!")
     }
 })
+
+function compareCards(card1, card2) {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
+    "10", "JACK", "QUEEN", "KING", "ACE"]
+    const val1 = valueOptions.indexOf(card1.value)
+    const val2 = valueOptions.indexOf(card2.value)
+    
+    if(val1 > val2) {
+        return "Computer wins!"
+    } else if(val1 < val2) {
+        return "You win!"
+    } else {
+        return "It's a War!"
+    }
+}
